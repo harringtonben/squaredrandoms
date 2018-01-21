0efe873931dbe61e5f50c1e0a8d2aa97ba3fd1af
Ben Harrington <harringtonben@icloud.com>
2018-01-21 10:08:42 -0600

    finished squared numbers

--- squaredrandoms/Program.cs
+++ squaredrandoms/Program.cs
@@ -10,6 +10,28 @@ namespace squaredrandoms
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            var randomNumbers = new List<int>();
+            var numbersToDisgard = new List<int>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                randomNumbers.Add(random.Next(1, 50));
+            }
+
+            for (var i =0; i < randomNumbers.Count; i++)
+            {
+                var squaredNumber = randomNumbers[i] * randomNumbers[i];
+                if (squaredNumber % 2 == 0)
+                {
+                    Console.WriteLine(squaredNumber);
+                }
+                else
+                {
+                    randomNumbers.RemoveAt(i);
+                }
+            }
+            Console.Read();
         }
     }
 }
